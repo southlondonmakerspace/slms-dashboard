abd6db4aa22ed79e5f6a8c7ccdfc2d23d3a5a0f5
Chris Roberts <chris@naxxfish.eu>
2017-02-27 11:53:00 +0000

    Initial commit - no UI yet, but National Rail and TFL proxies configured

---
+++ config.example.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'nrSecurityToken': 'my-securitty-token',
+    'nrLDBWSurl': 'https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2016-02-16',
+    'busStopNAPTANIDs': [
+        '490001149B',
+        '490015248C',
+        '490001149L',
+        '490001149N'
+    ],
+    'stationCRS': 'HNH'
+}
---
+++ index.js
@@ -0,0 +1,51 @@
+const config = require('./config')
+
+const DeparturesBoard = require('./lib/darwinDeparturesBoard')
+const TFLAPI = require('./lib/tflAPI')
+var depBoard = new DeparturesBoard(config.nrLDBWSurl, config.nrSecurityToken)
+var tfl = new TFLAPI()
+
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+
+
+router.get("/", (req, res) => {
+    res.sendFile(path + "index.html");
+});
+
+router.get('/trainDepartures', (req, res) => {
+    depBoard.lookupDepBoard(config.stationCRS).then(
+        (result) => {
+            res.end(JSON.stringify(result))
+        }).catch(
+        (error) => {
+            res.end('error' + error)
+        })
+})
+
+router.get('/busDepartures', (req, res) => {
+   var lookupPromises = []
+   config.busStopNAPTANIDs.forEach( (naptan) => {
+      lookupPromises.push(tfl.lookupBusDepartures(naptan))
+   })
+    Promise.all(lookupPromises).then(
+        (results) => {
+            res.end(JSON.stringify(results))
+        }
+    ).catch((error) => {
+        res.end('error:' + error)
+    })
+})
+
+
+app.use("/", router);
+
+app.use("*", (req, res) => {
+    res.sendFile(path + "404.html");
+});
+
+app.listen(3000, () => {
+    console.log("Live at Port 3000");
+});
