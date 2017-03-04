723dcef8468c2c012510830a66484d87587fb79a
Chris Roberts <chris@naxxfish.eu>
2017-03-04 23:43:59 +0000

    Angularised bus departures

--- widgets/busDepartures/public/script.js
+++ widgets/busDepartures/public/script.js
@@ -1,38 +1,37 @@
+var dashboard = angular.module('slmsDashboard')
 
-widgetUpdates.push(function() {
+dashboard.controller('BusDepartureController', ['$scope', '$http', '$interval',
+    function($scope, $http, $interval) {
+      $scope.stops = []
+        $http.get('/busStops').then(function(result) {
+            var stopList = result.data
+            // keep a log of all the requests being made, so we can callback when they're all done
+            var departureRequests = []
+            // create a board of departures for each bus stop
+            var departureBoard = {}
+            $scope.stops = []
+            stopList.forEach(function(stop) {
+                if (stop.hasOwnProperty('additionalProperties')) {
+                    stop.additionalProperties.forEach(function(property) {
+                        if (property.key == 'Towards') {
+                            stop.towards = property.value
 
-    $.getJSON('/busStops', {}, function(data) {
-        console.log('busDepartures: running')
+                        }
+                    })
+                }
+                $scope.stops.push(stop)
+            })
+            console.log('busStops',$scope.stops)
+            updateDepartures()
+        })
 
-        var stopList = data
-
-        // keep a log of all the requests being made, so we can callback when they're all done
-        var departureRequests = []
-        // create a board of departures for each bus stop
-        var departureBoard = {}
-        stopList.forEach(function(stop) {
-            // get the towards direction
-            var towards = ""
-            if (stop.hasOwnProperty('additionalProperties')) {
-                stop.additionalProperties.forEach(function(property) {
-                    if (property.key == 'Towards') {
-                        towards = property.value
-                    }
-                })
-            }
-            stop.towards = towards
-            //console.log(stop.indicator + " : " + stop.commonName + " towards " + towards)
-
-
-            console.log("queuing request to " + '/busDeparturesList/' + stop.naptanId)
-            departureRequests.push($.getJSON('/busDeparturesList/' + stop.naptanId, {}, function(departureData) {
-               console.log("returned request to " + '/busDeparturesList/' + stop.naptanId)
-               var stopHeader = $('<h3>')
-               stopHeader.append($('<span>' + stop.stopLetter + '</span>').addClass('circle-icon red'))
-               stopHeader.append($('<span>' + stop.commonName + " towards " + towards + '</span>').addClass('busTowards'))
-               console.log("from stop " + stop.stopLetter + " : " + stop.commonName)
-                var departures = departureData
-                //console.log(departures)
+        function updateDepartures() {
+           var departureRequests = []
+           $scope.stops.forEach(function (stop, index) {
+             $scope.stops[index].departures = []
+             departureRequests.push($http.get('/busDeparturesList/' + stop.naptanId).then(function (result) {
+                console.log(result)
+                var departures = result.data
                 var services = {}
                 departures.forEach(function(departure) {
                     var serviceKey = departure.lineId + ":" + departure.destinationName
@@ -41,14 +40,6 @@ widgetUpdates.push(function() {
                     }
                     services[serviceKey].push(departure)
                 })
-
-                var lineName, towards
-               var departureList = $('<ul id="' + stop.stopLetter + "-" + stop.stopLetter + '">')
-               departureBoard[stop.stopLetter + ":" + stop.commonName] = {
-                  'stop': stop,
-                  'departures': []
-               }
-               ,
                 Object.keys(services).forEach(function(serviceKey) {
                     var timesUntil = []
                     var service = services[serviceKey]
@@ -65,46 +56,27 @@ widgetUpdates.push(function() {
                         i--
                         untilString += Math.floor(time / 60) + " "
                         if (i != 0) {
-                            if (i == 1) {
+                           if (i == 1) {
                                 untilString += " and "
-                            } else {
+                           } else {
                                 untilString += ", "
-                            }
+                           }
                         }
                     })
-                    departureBoard[stop.stopLetter + ":" + stop.commonName].departures.push({
-                       'lineName': lineName,
-                       'destination': destination,
-                       'until': untilString
+                    $scope.stops[index].departures.push({
+                        'lineName': lineName,
+                        'destination': destination,
+                        'until': untilString
                     })
                     console.log(lineName, "to", destination, "in", untilString)
 
-                })
-            }))
-        })
-        $.when.apply(this,departureRequests).done(function () {
-           console.log("all requests done")
-           console.log(departureBoard)
-           $('#busDepartures').empty()
-          Object.keys(departureBoard).sort().forEach(function (stopKey) {
-             var stop = departureBoard[stopKey].stop
-             var stopHeader = $('<h3>')
-             stopHeader.append($('<span>' + stop.stopLetter + '</span>').addClass('circle-icon red'))
-             stopHeader.append($('<span>' + stop.commonName + " towards " + stop.towards + '</span>').addClass('busTowards'))
-             $('#busDepartures').append(stopHeader)
-             var departures = departureBoard[stopKey].departures
-             var departureList =  $('<ul>').addClass('busDepartureList')
-             departures.forEach(function (departure) {
-                var departureLine = $('<li>').addClass('busStopDeparture')
-                departureLine.append($('<span>' + departure.lineName + '</span>').addClass('busNumber'))
-                departureLine.append($("<span> to " + departure.destination + "</span>").addClass('busTowards'))
-                departureLine.append($("<span> in " + departure.until + " minutes </span>"))
-                departureList.append(departureLine)
-             })
-             $('#busDepartures').append(departureList)
+               })
+             }))
           })
-           //$('#busDepartures').append(departureBoard)
-        })
+        }
 
-    })
-})
+        $interval(function() {
+            updateDepartures()
+        }, 30000)
+    }
+])
