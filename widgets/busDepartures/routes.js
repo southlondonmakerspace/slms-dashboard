e86ba736f950bb31701820e97e8eace989957d1c
Chris Roberts <chris@naxxfish.eu>
2017-03-05 15:13:30 +0000

    Styling changing

--- widgets/busDepartures/routes.js
+++ widgets/busDepartures/routes.js
@@ -3,7 +3,7 @@ const config = require('../../config')
 module.exports = function (router) {
    var tfl = new TFLAPI(config.tfl.app_id,config.tfl.app_key)
    router.get('/busStops', (req, res) => {
-       tfl.lookupBusNAPTANStopPoints(config.lat, config.lon, 150).then((result) => {
+       tfl.lookupBusNAPTANStopPoints(config.lat, config.lon, config.radius).then((result) => {
            res.end(JSON.stringify(result))
        }).catch((error) => {
            res.end(500, error)
