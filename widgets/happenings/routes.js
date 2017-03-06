c11d0cf3ce28a0ed40bfe8f6303ec0dbba6bd8d1
Chris Roberts <chris@naxxfish.eu>
2017-03-06 18:12:36 +0000

    happenings

--- widgets/happenings/routes.js
+++ widgets/happenings/routes.js
@@ -3,9 +3,10 @@ const Happenings = require('../../lib/happenings')
 module.exports = function (router) {
    var happenings = new Happenings(config.icalAddress)
    router.get('/nowAndNext', (req, res) => {
-      console.log("getting", config.icalAddress)
       happenings.nowAndNext().then(function (result ) {
          res.end(JSON.stringify(result))
+      }).catch(function (err) {
+         res.end(JSON.stringify({'error':err}))
       })
    })
 }
