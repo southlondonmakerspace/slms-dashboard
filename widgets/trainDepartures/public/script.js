e1731ff25accb13c0d53c67a2f3f8e17d1d68892
Chris Roberts <chris@naxxfish.eu>
2017-03-06 23:42:24 +0000

    Less visual information

--- widgets/trainDepartures/public/script.js
+++ widgets/trainDepartures/public/script.js
@@ -10,7 +10,7 @@ dashboard.controller('TrainDepartureController', ['$scope', '$http', '$interval'
         function updateBoard() {
             $http.get('/trainDepartures').then(function(result) {
                 var board = result.data.GetStationBoardResult
-                if (board == undefined) {
+                if (board == undefined || result.data == undefined) {
                     console.error('[trainDepartures] invalid board recieved', result.data)
                     return
                 }
@@ -33,6 +33,20 @@ dashboard.controller('TrainDepartureController', ['$scope', '$http', '$interval'
                         } else {
                             modified.destinationName = departure.destination.location.locationName
                         }
+                        var relativeTime = moment(modified.std,"HH:mm")
+                        // if the time is in the past, then either the train is delayed, or it's actually talking about a train that departs after midnight
+                        if (relativeTime.isBefore(moment()))
+                        {
+                           // right. if the hour is before 4AM, then it's talking about a train after midnight
+                           if (relativeTime.hour() <= 4)
+                           {
+                              relativeTime.add(1,'day')
+                           }
+                           // otherwise, it actually IS the time it says it is
+                        }
+                        modified.relativeTime = relativeTime.fromNow(true)
+                        console.log('[trainDepartures]','departure',modified.std,modified.relativeTime)
+
                         departures.push(modified)
                     })
                 }
