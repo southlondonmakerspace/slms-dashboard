d555738a8efa9c3fa6794f2b0d7700448cea80cf
Chris Roberts <chris@naxxfish.eu>
2017-03-06 15:51:22 +0000

    Grouping bus stops by name

--- widgets/busDepartures/public/script.js
+++ widgets/busDepartures/public/script.js
@@ -2,16 +2,12 @@ var dashboard = angular.module('slmsDashboard')
 
 dashboard.controller('BusDepartureController', ['$scope', '$http', '$interval',
     function($scope, $http, $interval) {
-      console.log('[busDepartures]: getting list of bus stops')
+        console.log('[busDepartures]: getting list of bus stops')
         $scope.stops = []
         $http.get('/busStops').then(function(result) {
-           console.log('[busDepartures]: bus stops retrieved')
+            console.log('[busDepartures]: bus stops retrieved')
             var stopList = result.data
-            // keep a log of all the requests being made, so we can callback when they're all done
-            var departureRequests = []
-            // create a board of departures for each bus stop
-            var departureBoard = {}
-            $scope.stops = []
+            $scope.stops = {}
             stopList.forEach(function(stop) {
                 if (stop.hasOwnProperty('additionalProperties')) {
                     stop.additionalProperties.forEach(function(property) {
@@ -20,8 +16,12 @@ dashboard.controller('BusDepartureController', ['$scope', '$http', '$interval',
                         }
                     })
                 }
-                console.log(stop.commonName)
-                $scope.stops.push(stop)
+                if ($scope.stops[stop.commonName] == undefined) {
+                   $scope.stops[stop.commonName] = {}
+                   $scope.stops[stop.commonName].name = stop.commonName
+                    $scope.stops[stop.commonName].stations = []
+                }
+                $scope.stops[stop.commonName].stations.push(stop)
             })
             // sort the stops by name
             /*$scope.stops.sort(function (a, b){
@@ -31,75 +31,81 @@ dashboard.controller('BusDepartureController', ['$scope', '$http', '$interval',
                return 1
                return 0
             })*/
+            console.log('busDepartures scope',$scope.stops)
             updateDepartures()
         })
 
         function updateDepartures() {
-           console.log('[busDepartures]', 'updating')
-            var departureRequests = []
-            $scope.stops.forEach(function(stop, index) {
-                departureRequests.push($http.get('/busDeparturesList/' + stop.naptanId).then(function(result) {
-                    var departures = result.data
-                    var services = {}
-                    departures.forEach(function(departure) {
-                        var serviceKey = departure.lineId + ":" + departure.destinationName
-                        if (services[serviceKey] == undefined) {
-                            services[serviceKey] = []
-                        }
-                        services[serviceKey].push(departure)
-                    })
-                    var newDepartures = []
-                    Object.keys(services).forEach(function(serviceKey) {
-                        var timesUntil = []
-                        var service = services[serviceKey]
-                        var destination
-                        service.forEach(function(departure) {
-                            lineName = departure.lineName
-                            destination = departure.destinationName
-                            timesUntil.push(departure.timeToStation)
+            console.log('[busDepartures]', 'updating')
+            Object.keys($scope.stops).forEach(function(stopGroupName, stopGroupIndex) {
+               var stopGroup = $scope.stops[stopGroupName]
+               console.log('[busDepartures]','looking up departures for ', stopGroupName)
+                stopGroup.stations.forEach(function(stop, stopIndex) {
+                   console.log('[busDepartures]','looking up specific departures for ', stop)
+                    $http.get('/busDeparturesList/' + stop.naptanId).then(function(result) {
+                        var departures = result.data
+                        var services = {}
+                        departures.forEach(function(departure) {
+                            var serviceKey = departure.lineId + ":" + departure.destinationName
+                            if (services[serviceKey] == undefined) {
+                                services[serviceKey] = []
+                            }
+                            services[serviceKey].push(departure)
                         })
+                        var newDepartures = []
+                        Object.keys(services).forEach(function(serviceKey) {
+                            var timesUntil = []
+                            var service = services[serviceKey]
+                            var destination
+                            service.forEach(function(departure) {
+                                lineName = departure.lineName
+                                destination = departure.destinationName
+                                timesUntil.push(departure.timeToStation)
+                            })
 
-                        var untilString = ""
+                            var untilString = ""
 
-                        timesUntil.sort(function (a,b) { return a-b;})
-                        var earliestTime = timesUntil[0]
-                        var numTimes = 0;
-                        var i = 0
-                        const MAX_TIMES_TO_SHOW = 2
-                        var numTimesToShow = (timesUntil.length <= MAX_TIMES_TO_SHOW) ? timesUntil.length : MAX_TIMES_TO_SHOW;
-                        for (i=0;i<numTimesToShow;i++)
-                        {
-                           var time = timesUntil[i]
-                            untilString += Math.floor(time / 60) + " "
-                            if (i < numTimesToShow) {
-                                if (i == numTimesToShow-2) {
-                                    untilString += " and "
-                                } else {
-                                   if (i != numTimesToShow-1)
-                                   {
-                                      untilString += ", "
-                                   }
+                            timesUntil.sort(function(a, b) {
+                                return a - b;
+                            })
+                            var earliestTime = timesUntil[0]
+                            var numTimes = 0;
+                            var i = 0
+                            const MAX_TIMES_TO_SHOW = 2
+                            var numTimesToShow = (timesUntil.length <= MAX_TIMES_TO_SHOW) ? timesUntil.length : MAX_TIMES_TO_SHOW;
+                            for (i = 0; i < numTimesToShow; i++) {
+                                var time = timesUntil[i]
+                                untilString += Math.floor(time / 60)
+                                if (i < numTimesToShow) {
+                                    if (i == numTimesToShow - 2) {
+                                        untilString += " and "
+                                    } else {
+                                        if (i != numTimesToShow - 1) {
+                                            untilString += ", "
+                                        }
+                                    }
                                 }
                             }
-                        }
 
-                        newDepartures.push({
-                            'lineName': lineName,
-                            'destination': destination,
-                            'until': untilString,
-                            'earliestTime': earliestTime
+                            newDepartures.push({
+                                'lineName': lineName,
+                                'destination': destination,
+                                'until': untilString,
+                                'earliestTime': earliestTime
+                            })
                         })
+                        newDepartures.sort(function(a, b) {
+                            if (a.earliestTime < b.earliestTime)
+                                return -1;
+                            if (a.earliestTime > b.earliestTime)
+                                return 1;
+                            return 0;
+                        })
+                        console.log('[busDepartures] new departures', stopGroupName,stopIndex, newDepartures)
+                        $scope.stops[stopGroupName].stations[stopIndex].departures = newDepartures
                     })
-                    newDepartures.sort(function(a, b) {
-                        if (a.earliestTime < b.earliestTime)
-                            return -1;
-                        if (a.earliestTime > b.earliestTime)
-                            return 1;
-                        return 0;
-                    })
-                    console.log('[busDepartures] new departures', newDepartures)
-                    $scope.stops[index].departures = newDepartures
-                }))
+                    console.log($scope.stops)
+                })
             })
         }
 
