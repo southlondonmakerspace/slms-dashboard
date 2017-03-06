4ae637452daeab28640e2bd0476f9fa75c66c332
Chris Roberts <chris@naxxfish.eu>
2017-03-06 09:48:08 +0000

    tweak traindepartures

--- widgets/trainDepartures/public/script.js
+++ widgets/trainDepartures/public/script.js
@@ -4,37 +4,40 @@ dashboard.controller('TrainDepartureController', ['$scope', '$http', '$interval'
     function($scope, $http, $interval) {
         $scope.departures = []
         $interval(function() {
-           updateBoard()
+            updateBoard()
         }, 10000)
+
         function updateBoard() {
             $http.get('/trainDepartures').then(function(result) {
                 var board = result.data.GetStationBoardResult
                 if (board == undefined) {
-                    console.error('[trainDepartures] invalid board recieved',result.data)
+                    console.error('[trainDepartures] invalid board recieved', result.data)
                     return
                 }
                 if (board.nrccMessages != undefined) {
-                   $scope.message = ""
-                   $scope.showMessage = true
+                    $scope.message = ""
+                    $scope.showMessage = true
                     board.nrccMessages.message.forEach(function(message) {
                         $scope.message += "..." + message
                     })
                 } else {
-                   $scope.showMessage = false
+                    $scope.showMessage = false
                 }
                 $scope.locationName = board.locationName
                 var departures = []
-                board.trainServices.service.forEach(function(departure) {
-                    var modified = departure
-                    if (departure.destination.location.constructor === Array) {
-                        modified.destinationName = departure.destination.location[0].locationName
-                    } else {
-                        modified.destinationName = departure.destination.location.locationName
-                    }
-                    departures.push(modified)
-                })
+                if (board.trainServices != undefined) {
+                    board.trainServices.service.forEach(function(departure) {
+                        var modified = departure
+                        if (departure.destination.location.constructor === Array) {
+                            modified.destinationName = departure.destination.location[0].locationName
+                        } else {
+                            modified.destinationName = departure.destination.location.locationName
+                        }
+                        departures.push(modified)
+                    })
+                }
                 $scope.departures = departures
-                console.log('[trainDepartures] new departures',$scope.departures)
+                console.log('[trainDepartures] new departures', $scope.departures)
             })
         }
         updateBoard()
