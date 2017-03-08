2b05d4f7dc91d7a692393e461a722fef54bb6b58
Tom Lynch <me@unknowndomain.co.uk>
2017-03-08 12:10:10 +0000

    Updated config example to match presumed current config format

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
     'httpPort': 3000,
-    'nrSecurityToken': 'my-securitty-token',
+    'nrSecurityToken': '',
     'nrLDBWSurl': 'https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2016-02-16',
     'busStopNAPTANIDs': [
         '490001149B',
@@ -8,5 +8,12 @@ module.exports = {
         '490001149L',
         '490001149N'
     ],
-    'stationCRS': 'HNH'
+    lat: '51.4519181',
+    lon: '-0.1008958',
+    radius: '250',
+    'stationCRS': 'HNH',
+    'tfl': {
+        appId: "",
+        appKey: ""
+    }
 }
