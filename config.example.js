4deada75ac0c1aa43be4f151a4ed87049d57051e
unknowndomain <me@unknowndomain.co.uk>
2017-03-08 12:16:23 +0000

    Correction to config example

--- config.example.js
+++ config.example.js
@@ -1,19 +1,14 @@
 module.exports = {
     'httpPort': 3000,
-    'nrSecurityToken': '',
-    'nrLDBWSurl': 'https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2016-02-16',
-    'busStopNAPTANIDs': [
-        '490001149B',
-        '490015248C',
-        '490001149L',
-        '490001149N'
-    ],
-    lat: '51.4519181',
-    lon: '-0.1008958',
-    radius: '250',
-    'stationCRS': 'HNH',
-    'tfl': {
-        appId: "",
-        appKey: ""
-    }
-}
+     'nrSecurityToken': 'xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx', // this is a security token from OpenLDBWS
+     'nrLDBWSurl': 'https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2016-02-16', // this is the SOAP endpoint for LWDB
+     'tfl': {
+       'appId': 'xxxxx',
+       'appKey': 'xxxxx'
+    },
+     'lat': 51.451860, // latitude and longitude of the space
+     'lon': -0.100861,
+	 'radius': 250,
+     'stationCRS': 'HNH',// this is the CRS code of the station you want to display the departure board for
+     'icalAddress': 'https://calendar.google.com/calendar/ical/6hnjp743rq7omi2qfr3fa873ug%40group.calendar.google.com/public/basic.ics'
+ }
